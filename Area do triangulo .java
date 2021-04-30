afa89ed87bc4136c48a825276d03e03fb55bd8cc
João Antônio <77999403+jads15@users.noreply.github.com>
2021-04-30 17:08:15 -0300

    Update Area do triangulo .java

--- Area do triangulo .java
+++ Area do triangulo .java
@@ -1,1 +1,20 @@
+package com.company.java;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+    	float B;
+	    float H;
+	    System.out.println("Digite a base do triângulo:");
+	    B = scan.nextFloat();
+	    System.out.println("Digite a altura do triangulo:");
+	    H = scan.nextFloat();
+	     float area = B*H/2;
+	     System.out.println("a area do triangulo é: "+ area+"cm²");
+
+
+
+    }
+}
 
